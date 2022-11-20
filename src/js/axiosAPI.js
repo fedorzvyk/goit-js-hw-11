3fc5faa3a6883e514de8fea1084d93c59cdd9adb
fedorzvyk <fedorzvyk@gmail.com>
2022-11-21 00:22:34 +0200

    fix bug

--- src/js/axiosAPI.js
+++ src/js/axiosAPI.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const BASE_URL = 'https://pixabay.com/api/';
 const KEY = '31396725-3e8f0bf1315c05b54d621a44f';
-PER_PAGE = 40;
+const PER_PAGE = 40;
 
 export async function getImages(name, page) {
   try {
